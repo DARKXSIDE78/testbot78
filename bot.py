16d22eba289bc5c4ff862f1a0b1c085db0f8d375
DARKXSIDE78 <nk1010057@gmail.com>
2025-02-11 15:03:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -30,7 +30,6 @@ api_id = '29478593'
 api_hash = '24c3a9ded4ac74bab73cbe6dafbc8b3e'
 bot_token = '7426089831:AAFCCHq9EBxyt2LCj4irZ6As-UyGUnHN7zg'
 url_d = 'https://myanimelist.net/rss/news.xml'
-url_e = 'https://www.livechart.me/feeds/headlines'
 start_pic = "https://images5.alphacoders.com/113/thumb-1920-1134698.jpg"
 ANILIST_API_URL = 'https://graphql.anilist.co'
 
@@ -278,7 +277,7 @@ async def fetch_and_send_news():
 
     news_channel = "@" + config["news_channel"]
 
-    for url in [url_d, url_e]:
+    for url in [url_d]:
         feed = await asyncio.to_thread(feedparser.parse, url)
         for entry in feed.entries:
             entry_id = entry.get('id', entry.get('link'))
