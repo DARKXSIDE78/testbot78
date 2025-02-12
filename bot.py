4cd288a7ecf376003c93b0f7dccf9f739547e819
DARKXSIDE78 <nk1010057@gmail.com>
2025-02-12 13:03:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -86,9 +86,8 @@ async def anime(client, message):
         return
 
     anime_name = " ".join(message.text.split()[1:])
-    template, cover_image = await get_anime_data(anime_name, language, subtitle, season)
-    await send_message_to_user(chat_id, template, cover_image)
-
+    template, cover_image = await get_anime_data(anime_name, language, subtitle, season, global_settings_collection)
+    await send_message_to_user(app, chat_id, template, cover_image)
 
 @app.on_message(filters.command("manga"))
 async def manga(client, message):
