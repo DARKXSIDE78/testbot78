4912f89d7d85ab5d029061b8911de7b9e46631e3
DARKXSIDE78 <nk1010057@gmail.com>
2025-02-13 10:16:56 +0530

    Update manga.py

--- template/manga.py
+++ template/manga.py
@@ -63,19 +63,23 @@ async def get_manga_data(manga_name: str, language: str, global_settings_collect
                         if manga.get("endDate") and None not in [manga['endDate'].get('year'), manga['endDate'].get('month'), manga['endDate'].get('day')]
                         else "Null"
                     )
+
+                    status = manga["status"]
                     
                     # Safe fetching for volumes and chapters, fall back to "N/A" if None
-                    volumes = manga.get("volumes", "N/A")
-                    chapters = manga.get("chapters", "N/A")
+                    volumes = manga["volumes"] if manga["volumes"] else "N/A"
+                    chapters = manga["chapters"] if manga["chapters"] else "N/A"
                     
                     # Safe fetching for genres, fall back to "N/A" if None
-                    genres = ', '.join(manga["genres"]) if manga.get("genres") else "N/A"
+                    genres = ', '.join(manga["genres"]) if manga["genres"] else "N/A"
                     
                     manga_id = manga.get("id")
 
                     manga_hub = (global_settings_collection.find_one({'_id': 'config'}) or {}).get('manga_hub', '@FraxxManga')
 
                     cover_url = await get_manga_cover(manga_id)
+
+                    
 
                     template = f"""
 **{title}**
